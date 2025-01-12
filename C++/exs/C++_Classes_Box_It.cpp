ccb8ce079bc24614573e9b242f131f11956d52cc
Sulaiman3352 <32817650+Sulaiman3352@users.noreply.github.com>
2025-01-12 18:24:12 +0300

    Add files via upload

---
+++ C++/exs/C++_Classes_Box_It.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//Implement the class Box  
+//l,b,h are integers representing the dimensions of the box
+
+// The class should have the following functions : 
+
+// Constructors: 
+class Box{
+private:
+    int length, breadth, height;
+    
+public:
+    Box(): length(0),breadth(0),height(0){};
+    Box(int length, int breadth, int height)
+      : length(length), breadth(breadth), height(height) {}
+
+Box(const Box& B);
+
+
+int getLength() const {return length;} // Return box's length
+int getBreadth () const{return breadth;} // Return box's breadth
+int getHeight () const{return height;}  //Return box's height
+long long CalculateVolume() const{return length * breadth * breadth;} // Return the volume of the box
+
+//Overload operator < as specified
+//bool operator<(Box& b)
+
+//Overload operator << as specified
+//ostream& operator<<(ostream& out, Box& B)
+};
+
+void check2()
+{
+	int n;
+	cin>>n;
+	Box temp;
+	for(int i=0;i<n;i++)
+	{
+		int type;
+		cin>>type;
+		if(type ==1)
+		{
+			cout<<temp<<endl;
+		}
+		if(type == 2)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			temp=NewBox;
+			cout<<temp<<endl;
+		}
+		if(type==3)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			if(NewBox<temp)
+			{
+				cout<<"Lesser\n";
+			}
+			else
+			{
+				cout<<"Greater\n";
+			}
+		}
+		if(type==4)
+		{
+			cout<<temp.CalculateVolume()<<endl;
+		}
+		if(type==5)
+		{
+			Box NewBox(temp);
+			cout<<NewBox<<endl;
+		}
+
+	}
+}
+
+int main()
+{
+	check2();
+}
